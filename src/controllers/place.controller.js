24c4a54b149c47c34097cff7d7f11a9d78be40f5
abhishek36 <akamble9913@gmail.com>
2022-02-04 17:42:31 +0530

    first commit

---
+++ src/controllers/place.controller.js
@@ -0,0 +1,32 @@
+//import Place model here
+
+const addPlace = async (req, res) => {
+    //addPlace api logic here
+};
+
+const getAllPlaceByCityAndCategoryId = async (req, res) => {
+    //getAllPlaceByCityAndCategoryId api logic here
+};
+
+const getPlaceById = async (req, res) => {
+    //getPlaceById api logic here
+};
+
+const updatePlaceById = async (req, res) => {
+    //updatePlaceById api logic here
+};
+
+const deletePlaceById = async (req, res) => {
+    //deletePlaceById api logic here
+};
+
+
+const PlaceController = {
+    addPlace,
+    getAllPlaceByCityAndCategoryId,
+    getPlaceById,
+    updatePlaceById,
+    deletePlaceById
+};
+
+module.exports = PlaceController;
